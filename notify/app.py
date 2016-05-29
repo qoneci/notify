0a4f9a1c080169af3fbe3943b1e20239ae7470f2
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-05-29 22:48:24 +0200

    api: remove middleware in favour of decorators

--- notify/app.py
+++ notify/app.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 import falcon
 from notify.core import handlers
-from notify.core.middleware import JSONTranslator, RequireJSON
-
-
-api = falcon.API(middleware=[
-    RequireJSON(),
-    JSONTranslator(),
-])
 
 
 api = falcon.API()
