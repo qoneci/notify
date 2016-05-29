981c44651c91c399c2d613f75fc67a875ede88fe
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-05-29 11:43:58 +0200

    core: handlers: adding init setup to get health and start of post notify message

---
+++ notify/core/handlers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import falcon
+from notify.core.log import logger
+
+
+class GetHealth:
+    def __init__(self):
+        self.log = logger()
+
+    def on_get(self, req, resp):
+        resp.body = '{"status": "OK"}'
+
+
+class NotifyEvent:
+    def __init__(self):
+        self.log = logger()
+
+    def on_post(self, req, resp):
+        try:
+            doc = req.context['doc']
+        except KeyError:
+            raise falcon.HTTPBadRequest('must be submitted in the request body.')
+
+        resp.status = falcon.HTTP_201
