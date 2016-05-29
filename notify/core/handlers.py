d6c2935c96057cd66962992d0492c28cd6926564
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-05-29 22:47:55 +0200

    change to use decorators

--- notify/core/handlers.py
+++ notify/core/handlers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import json
 import falcon
+from notify.core import hocks
 from notify.core import validator
 from notify.core.log import logger
 from notify.plugins.services import Slack
@@ -19,38 +19,39 @@ class NotifyEvent:
     def __init__(self):
         self.log = logger()
 
+    @falcon.before(hocks.require_json)
+    @falcon.before(hocks.json_translator)
     def on_post(self, req, resp):
+        doc = None
         try:
-            print('req.context: {}'.format(req.context))
-            body = req.stream.read()
-            req.context['doc'] = json.loads(body.decode('utf-8'))
             doc = req.context['doc']
-            if isinstance(doc.get('services'), list):
-                checks = validator.notify_event(doc, services=doc.get('services'))
-                if False not in checks:
-                    services = doc.get('services')
-                    if 'slack' in services:
-                        slack_client = Slack(doc.get('org_name'))
-                        slack_client.init_client()
-                        slack_client.send_message(
-                            doc.get('channel_name'),
-                            doc.get('message')
-                        )
-                    if 'datadog' in services:
-                        dd = DataDog(doc.get('org_name'))
-                        dd.init_client()
-                        dd.send_event(
-                            doc.get('alert_type'),
-                            doc.get('message'),
-                            tags=doc.get('tags'),
-                            alert_type=doc.get('alert_type'),
-                        )
-                else:
-                    raise falcon.HTTPBadRequest('Checks failed {}'.format(checks))
-            else:
-                raise falcon.HTTPBadRequest('Missing Service in doc {}'.format(doc))
-
         except KeyError:
-            raise falcon.HTTPBadRequest('must be submitted in the request body.')
+            raise falcon.HTTPBadRequest('JSONTranslator failed',
+                                        'JSON must be submitted in the request body.')
+
+        if isinstance(doc.get('services'), list):
+            checks = validator.notify_event(doc, services=doc.get('services'))
+            if False not in checks:
+                services = doc.get('services')
+                if 'slack' in services:
+                    slack_client = Slack(doc.get('org_name'))
+                    slack_client.init_client()
+                    slack_client.send_message(
+                        doc.get('channel_name'),
+                        doc.get('message')
+                    )
+                if 'datadog' in services:
+                    dd = DataDog(doc.get('org_name'))
+                    dd.init_client()
+                    dd.send_event(
+                        doc.get('alert_type'),
+                        doc.get('message'),
+                        tags=doc.get('tags'),
+                        alert_type=doc.get('alert_type'),
+                    )
+            else:
+                raise falcon.HTTPBadRequest('Checks failed {}'.format(checks))
+        else:
+            raise falcon.HTTPBadRequest('Missing Service in doc {}'.format(doc))
 
         resp.status = falcon.HTTP_201
